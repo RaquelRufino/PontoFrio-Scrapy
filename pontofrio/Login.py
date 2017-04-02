762c5dec3d599ea9c05f4129bccd892516f98481
wellington16 <well@wel.com>
2017-04-02 00:40:29 -0300

    Scrapy- Ajustando Login

--- pontofrio/Login.py
+++ pontofrio/Login.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+import logging
 
 from scrapy.spiders import Spider
 from scrapy.utils.response import open_in_browser
@@ -9,7 +10,7 @@ from scrapy.utils.response import open_in_browser
 '''
     UNIVERSIDADE FEDERAL RURAL DE PERNAMBUCO - UFRPE
     Curso: Bacharelado em Sistemas de Informação
-    Scrapy - Login
+    Scrapy - Login no Linkedin
     Retornar response com as informações da conta logada.
     Executado em python 2.7
 '''
@@ -23,17 +24,17 @@ class LoginSpider(Spider):
     def parse(self, response):
         return scrapy.FormRequest.from_response(
             response,
-            formdata={'session_key': 'email@gmail.com',
-                      'session_password': 'senha'},
+            formdata={'session_key': 'Seu email',
+                      'session_password': 'Sua senha'},
             callback=self.depois_do_login
         )
 
     def depois_do_login(self, response):
         # check login succeed before going on
         if 'login-error' in response.body:
-            self.logger.error("Falhou ao logar. ")
+            logging.error("Falha ao logar. ")
         else:
-            self.logger.info("Logado com sucesso")
+            logging.info("Logado com sucesso")
         yield scrapy.Request(url='https://www.linkedin.com/feed/?trk=hb_signin', callback=self.parse_detail)
 
     def parse_detail(self, response):
